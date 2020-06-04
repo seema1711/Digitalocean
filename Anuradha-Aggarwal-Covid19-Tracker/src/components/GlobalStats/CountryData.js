14f70643677328eb9232b0cc9651450298f69882
anuradha9712 <anuradha.mcs18.du@gmail.com>
2020-06-05 03:37:00 +0530

    edit README

--- Anuradha-Aggarwal-Covid19-Tracker/src/components/GlobalStats/CountryData.js
+++ Anuradha-Aggarwal-Covid19-Tracker/src/components/GlobalStats/CountryData.js
@@ -10,15 +10,15 @@ const CountryData = ({countryName , allData }) => {
 
         return (
             <div>
-
                 <div>
                     {countryToShow.map(item => (
                         <div key={item.Country}>
                             <h4>{item.Country}</h4>
                             <div className="row">
-                                <Display key={item.TotalConfirmed} name="Total Confirmed" number={item.TotalConfirmed} />
-                                <Display key={item.TotalDeaths} name="Total Deaths" number={item.TotalDeaths} />
-                                <Display key={item.TotalRecovered} name="Total Recovered" number={item.TotalRecovered} />
+                                <Display key={item.TotalConfirmed} name="Total Confirmed" number={item.TotalConfirmed} color="info" font="white" />
+                                <Display key={item.TotalRecovered} name="Total Recovered" number={item.TotalRecovered} color="success" font="white" />
+                                <Display key={item.TotalDeaths} name="Total Deaths" number={item.TotalDeaths} color="danger" font="white" />
+
                             </div>
                         </div>
                     ))}
